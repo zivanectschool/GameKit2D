e8bc2b7c1f23b39047de13f43e2bf1c5ba405463
InvisibleAnonymous <joris.baranauskas@gmail.com>
2020-02-08 14:41:13 +0200

    joris stuff

---
+++ GameKit2D/Assets/2DGamekit/Scripts/Script_HeavenBOSSS.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_HeavenBOSSS : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private GameObject[] spawners;
+
+    [SerializeField]
+    private int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        spawners = GameObject.FindGameObjectsWithTag("Spawn");
+
+        int rand = UnityEngine.Random.Range(0, spawners.Length);
+
+        transform.position = spawners[rand].transform.position + new Vector3(0.0f, 5.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0) Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("bullet"))
+        {
+            DecreaseHealth(1);
+        }
+    }
+
+    public void DecreaseHealth(int index)
+    {
+        health -= 1;
+    }
+}
